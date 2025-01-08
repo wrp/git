60a21188db3aa7aa00d2fa00fe5708fc18e35186
William Pursell <william.r.pursell@rubrik.com>
2025-01-08 11:12:40 -0700

    Short circuit usage_with_options to avoid spew

--- parse-options.c
+++ parse-options.c
@@ -1299,6 +1299,7 @@ static enum parse_opt_result usage_with_options_internal(struct parse_opt_ctx_t 
 	FILE *outfile = err ? stderr : stdout;
 	int need_newline;
 
+	return PARSE_OPT_HELP;
 	const char *usage_prefix = _("usage: %s");
 	/*
 	 * The translation could be anything, but we can count on
